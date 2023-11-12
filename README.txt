f95bc90ecaefb0ec4275abafb2ceb5b9fb8f2c8b
Stack-overflow-abuser <mmd.miguel@gmail.com>
2023-11-12 13:45:14 +0000

    Almost done (first week)

--- README.txt
+++ README.txt
@@ -1,2 +1,23 @@
 Dataset: https://www.kaggle.com/datasets/jessemostipak/hotel-booking-demand/data
-Lines: 119390 linhas
+Lines: 119390 Lines
+
+Table1:{
+    name: Main_Info
+    colunas: hotel, lead_time, arrival_date_year, arrival_date_month, arrival_date_week_number,
+    arrival_date_day_of_month, country, market_segment, distribution_channel, is_repeated_guest, booking_changes, days_in_waiting_list, company, customer_type, reservation_status_date
+
+}
+
+
+Ver os meses e anos das reserva mais canceladas, comparar também pessoas de agencia cancelam ou nao as reservas, checking wich country cancells aswell as the deposit type
+Table2:{
+    name: Reservas Status
+    columns: is_cancelled, arrival_date_year, arrival_date_month, angent, previous_cancellations, previous_bookings_not_cancelled, country, deposit_type, reservation_status
+}
+
+
+Comparar o tempo da estadia de familias com filhos e sem filhos e ver se requisitam parque de estacionamento para carro
+Table3:{
+    name:
+    columns: stays_in_week_nights, stays_in_weekends_nights, adult, children, babies, required_car_parking_spaces, meal, reserved_room_type, total_of_special_requests
+}
--- script.py
+++ script.py
@@ -2,4 +2,28 @@ import pandas as pd
 
 df = pd.read_csv('hotel_bookings.csv')
 
-df.to_json('jsonFile.json', orient='records', lines=True)
+
+columns = ["hotel", "lead_time", "arrival_date_year", "arrival_date_month", "arrival_date_week_number","arrival_date_day_of_month", 
+           "country", "market_segment", "distribution_channel", "is_repeated_guest", "booking_changes", "days_in_waiting_list", "company",
+             "customer_type", "reservation_status_date"]
+
+columns1= ["is_canceled", "arrival_date_year", "arrival_date_month", "agent", "previous_cancellations", "previous_bookings_not_canceled", "country", "deposit_type", "reservation_status"]
+
+columns2 = ["stays_in_week_nights", "stays_in_weekend_nights", "adults", "children", "babies", "required_car_parking_spaces", "meal", "reserved_room_type", "total_of_special_requests"]
+
+df_selected = df[columns]
+
+
+df_selected1 = df[columns1]
+
+
+df_selected2 = df[columns2]
+
+df_selected.to_csv("Tabela1.csv", index=True)
+df_selected1.to_csv("Tabela2.csv", index=True)
+df_selected2.to_csv("Tabela3.csv", index=True)
+
+df_selected.to_json("Tabela1.json",orient="records", lines=True)
+df_selected1.to_json("Tabela2.json",orient="records", lines=True)
+df_selected2.to_json("Tabela3.json",orient="records", lines=True)
+
