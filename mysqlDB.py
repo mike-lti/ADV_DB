a9b1ee6dbbd07e7f783237fc67db36d904e73a6d
Benjamim-V <126713479+Benjamim-V@users.noreply.github.com>
2023-12-03 23:27:19 +0000

    Update mysqlDB.py

--- mysqlDB.py
+++ mysqlDB.py
@@ -116,16 +116,76 @@ df_stays_info_list = df_stays_info.values.tolist()
 # mycursor.executemany(stays_info_insert_query, df_stays_info_list)
 # mydb.commit()
 
+#Query simples 1 "Quais são os país que tiveram resevas superior a 1000 e, em que ano e mes que isto aconteceu e o hotel que mais requições teve?"
+start_time = time.time()
+
+# Consulta SQL corrigida
+sql_consulta = """
+    SELECT
+        hotel,
+        country,
+        arrival_date_year AS arrival_year,
+        arrival_date_month AS arrival_month,
+        COUNT(*) AS total_reservas
+    FROM
+        main_info 
+    GROUP BY
+        hotel, country, arrival_date_year, arrival_date_month
+    HAVING
+        COUNT(*) > 1000
+    ORDER BY
+        total_reservas DESC
+"""
+# Execute a consulta e armazena o resultado em um DataFrame
+df_sql = pd.read_sql(sql_consulta, con=engine)
+end_time = time.time()
+execution_time = end_time - start_time
+# Exiba o DataFrame resultante
+print(df_sql)
+print(f"Tempo de execução: {execution_time:.2f} segundos.")
+
+#Query simple 2 "Quais são os país que tiveram resevas alteradas suerior a 100 e, em que ano isto ocorreu?"
+#R:
+start_time = time.time()
+sqlConsult = """
+    SELECT
+    arrival_date_year,
+    country,
+    COUNT(*) AS total_reservas_alteradas
+FROM
+    main_info
+WHERE
+    booking_changes = 0 
+GROUP BY
+    arrival_date_year, country
+HAVING
+    COUNT(*) > 90
+ORDER BY
+    arrival_date_year;
+"""
+#A cláusula HAVING é usada em combinação com GROUP BY para filtrar os resultados de uma agregação com base a contagem das reservas.
+#Neste caso, estamos filtrando os resultados para incluir apenas as linhas onde o total de reservas é maior que 100.
+dfSql = pd.read_sql(sqlConsult, con=engine)
+end_time = time.time()
+execution_time = end_time - start_time
+#Estas linhas, permitem remover o numero total de linhas e colunas a serem exibidas 
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+# Exiba o DataFrame resultante
+print(dfSql)
+print(f"Tempo de execução: {execution_time:.2f} segundos.")
+
+
 #query complexa 1 casais sem filhos ou com filhos quem cancela mais reservas
 complex_query1 = """"
-SELECT *
-FROM main_info 
-JOIN stays_info ON main_info.id_reservation = stays_info.id_reservation
-JOIN reservas_status ON main_info.id_reservation = reservas_status.id_reservation
-WHERE stays_info.adults > 0
-  AND (stays_info.children > 0 OR stays_info.babies > 0)
-  AND reservas_status.reservation_status != 'Canceled'
-LIMIT 0, 50000;
+SELECT main_info.country, main_info.hotel, stays_info.adults, stays_info.children, reservas_status.reservation_status
+    FROM main_info 
+    JOIN stays_info ON main_info.id_reservation = stays_info.id_reservation
+    JOIN reservas_status ON main_info.id_reservation = reservas_status.id_reservation
+    WHERE stays_info.adults > 0
+      AND (stays_info.children > 0 OR stays_info.babies > 0)
+      AND reservas_status.reservation_status NOT LIKE 'Canceled'
+    LIMIT 0, 20;
 """
 
 try:
@@ -140,14 +200,15 @@ except mysql.connector.Error as err:
 
 #query complexa 2 casais sem filhos ou com filhos tem parqueamento e/ou refeições
 complex_query2 = """"
-SELECT *
-FROM main_info 
-JOIN stays_info ON main_info.id_reservation = stays_info.id_reservation
-WHERE stays_info.adults > 0 
-  AND (stays_info.children > 0 OR stays_info.babies > 0) 
-  AND (stays_info.required_car_parking_spaces > 0 
-    OR stays_info.meal != 'Undefined' 
-    OR stays_info.meal != 'SC') limit 5;
+SELECT main_info.country, main_info.hotel, stays_info.adults, stays_info.babies, stays_info.required_car_parking_spaces as car_parking , stays_info.children, stays_info.meal
+    FROM main_info 
+    JOIN stays_info ON main_info.id_reservation = stays_info.id_reservation
+    WHERE stays_info.adults > 0 
+      AND (stays_info.children > 0 OR stays_info.babies > 0) 
+      AND (stays_info.required_car_parking_spaces > 0 
+        OR stays_info.meal NOT LIKE 'Undefined' 
+        OR stays_info.meal NOT LIKE 'SC')
+    LIMIT 20;
 """
 
 
@@ -161,6 +222,130 @@ try:
 except mysql.connector.Error as err:
     print("Error executing query", err)
 
+
+#Actualização de dados na tabela Main_info
+# Novo valor para a coluna hotel
+novo_hotel = "City Hotel"
+start_time = time.time()
+# Consulta de atualização
+sql_update = text(f"""
+    UPDATE main_info
+    SET hotel = :novo_hotel
+    WHERE hotel = "7 Hotel"
+""")
+
+# Execute a consulta de atualização
+with engine.begin() as connection:
+    connection.execute(sql_update, {"novo_hotel": novo_hotel})
+end_time = time.time()
+execution_time = end_time - start_time
+print("Atualização concluída com sucesso.")
+print(f"Tempo de execução: {execution_time:.2f} segundos.")
+
+#Inserção de dados na tabela Main_info
+try:
+    start_time = time.time()
+
+    #Inserção de novos dados na tabela Main_Info
+    data = {
+        'hotel': ['BDA Hotel'],
+        'lead_time': [30],
+        'arrival_date_year': [2023],
+        'arrival_date_month': ['December'],
+        'arrival_date_week_number': [48],
+        'arrival_date_day_of_month': ['1'],
+        'country': ['PRT'],
+        'market_segment': ['Direct'],
+        'distribution_channel': ['Direct'],
+        'is_repeated_guest': [0],
+        'booking_changes': [2],
+        'days_in_waiting_list': [0],
+        'company': ['BDA'],
+        'customer_type': ['Transient'],
+        'reservation_status_date': ['2023-12-01']
+    }
+
+    df = pd.DataFrame(data)
+
+    # Inserção de dados na tabela 'Main_Info' usando SQLAlchemy
+    df.to_sql('main_info', con=engine, if_exists='append', index=False)
+
+    # Recupere o registro recém-inserido
+    query = "SELECT id_reservation,hotel,company,reservation_status_date FROM main_info WHERE hotel = 'BDA Hotel' LIMIT 1"
+    inserted_record = pd.read_sql(query, con=engine)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print("Inserção concluída com sucesso.")
+    print("Registro inserido:")
+    print(inserted_record)
+    print(f"Tempo de execução: {execution_time:.2f} segundos.")
+
+except Exception as e:
+    print(f"Erro durante a inserção: {str(e)}")
+
+#Inserção de novos dados nas tabela que contem chaves estrangeiras(reservas_status e stays_info)
+
+start_time = time.time()
+
+#Novos dados a serem inseridos
+data1 = {
+    'id_reservation': [119397],
+    'is_canceled': [0],
+    'arrival_date_year': [2023],
+    'arrival_date_month': ['December'],
+    'agent': [48],
+    'previous_cancellations': [1],
+    'previous_bookings_not_canceled': [0],
+    'country': ['PRT'],
+    'deposit_type': ['No Deposit'],
+    'reservation_status': ['Check-Out']
+}
+
+data2 = {
+    'id_reservation': [119397],
+    'stays_in_week_nights': [3],
+    'stays_in_weekend_nights': [2],
+    'adults': [2],
+    'children': [1],
+    'babies': [1],
+    'required_car_parking_spaces': [1],
+    'meal': ['BB'],
+    'reserved_room_type': ['B'],
+    'total_of_special_requests': [2]
+}
+
+# Inserção de dados nas tabelas 'reservas_status' e 'stays_info' usando SQLAlchemy
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+try:
+    df1.to_sql('reservas_status', con=engine, if_exists='append', index=False)
+    df2.to_sql('stays_info', con=engine, if_exists='append', index=False)
+    print("Novos dados inseridos com sucesso.")
+except exc.IntegrityError as e:
+    print(f"Erro:  O registro já existe na tabela.")
+ 
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Tempo de execução: {execution_time:.2f} segundos.")
+
+#Criação indices
+mycursor = mydb.cursor()
+
+# Índice para a tabela Main_Info
+mycursor.execute("CREATE INDEX idx_id_reservation_main ON main_info(id_reservation)")
+
+# Índice para a tabela Reservas_Status
+mycursor.execute("CREATE INDEX idx_id_reservation_reservas ON reservas_status(id_reservation)")
+
+# Índice para a tabela Stays_Info
+mycursor.execute("CREATE INDEX idx_id_reservation_stays ON stays_info(id_reservation)")
+
+# Confirmar as alterações no banco de dados
+mydb.commit()
+print("Índices criados com sucesso!")
 
 # Close the connection to the database
 mydb.close()
